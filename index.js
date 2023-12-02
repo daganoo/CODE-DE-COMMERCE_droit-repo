e15dbf5a885dbaec3ced497dcc800ea8d7ce4bdf
Marouane Dagana <136081716+daganoo@users.noreply.github.com>
2023-12-02 14:25:34 +0100

    some changes

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 function getText() {
-    var inputId = document.getElementById("inp").value;
+  var inputId = document.getElementById("inp").value;
 
-    var liElement = document.getElementById("a" + inputId);
+  var liElement = document.getElementById("a" + inputId);
 
-    var outputElement = document.getElementById("texteId");
+  var outputElement = document.getElementById("texteId");
 
-    if (liElement) {
-      outputElement.innerHTML =
-        "Phrase extraite : " + liElement.textContent;
-    } else {
-      outputElement.innerHTML = "Aucun élément trouvé avec cet ID.";
-    }
+  if (liElement) {
+    outputElement.innerHTML = "Phrase extraite : " + liElement.textContent;
+  } else {
+    outputElement.innerHTML = "Aucun élément trouvé avec cet ID.";
   }
+}
