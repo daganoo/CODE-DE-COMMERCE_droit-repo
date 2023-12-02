2712be04fbe338a6f92bed6cfa003af7b3737f93
Marouane Dagana <136081716+daganoo@users.noreply.github.com>
2023-12-02 14:06:59 +0100

    files uplode

---
+++ index.js
@@ -0,0 +1,14 @@
+function getText() {
+    var inputId = document.getElementById("inp").value;
+
+    var liElement = document.getElementById("a" + inputId);
+
+    var outputElement = document.getElementById("texteId");
+
+    if (liElement) {
+      outputElement.innerHTML =
+        "Phrase extraite : " + liElement.textContent;
+    } else {
+      outputElement.innerHTML = "Aucun élément trouvé avec cet ID.";
+    }
+  }
